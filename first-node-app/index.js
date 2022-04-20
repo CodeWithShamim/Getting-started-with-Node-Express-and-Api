bb4b2bc8cbed6c5e204b85c889b3125983e7fc6f
codewithshamim <shamimislamonline@gmail.com>
2022-04-20 22:25:18 +0600

    create dynamic api and access params

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -1,11 +1,32 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (request, response) => {
-    response.send("This is express");
+    response.send("This is express nodemon sssssssseeeeee");
+})
+
+const users = [
+    { id: 1, name: 'shamim', email: 'shamim@gmail.com' },
+    { id: 2, name: 'salman', email: 'salman@gmail.com' },
+    { id: 3, name: 'sakib', email: 'sakib@gmail.com' },
+    { id: 4, name: 'sojib', email: 'sojib@gmail.com' },
+    { id: 5, name: 'sadman', email: 'sadman@gmail.com' }
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params)
+    const id = parseInt(req.params.id);
+
+    const getUser = users.find(user => user.id === id);
+    res.send(getUser)
 })
 
 app.listen(port, () => {
-    console.log('Listening is port', port);
+    console.log('Listening to port', port);
 });
