ee42be5e962e635acc34c0524e26e2956fc4f507
codewithshamim <shamimislamonline@gmail.com>
2022-04-20 22:57:08 +0600

    successfully load data from server

--- React with Router/react-with-node/src/App.js
+++ React with Router/react-with-node/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  // console.log(users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users?.map((user) => (
+        <div key={user.id}>
+          <h2>Id: {user.id}</h2>
+          <h5>Name: {user.name}</h5>
+          <p>Email: {user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
