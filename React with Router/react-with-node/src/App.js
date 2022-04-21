240833196cef25594ee83f2ad94cb8553fff6353
codewithshamim <shamimislamonline@gmail.com>
2022-04-21 10:35:59 +0600

    display post data

--- React with Router/react-with-node/src/App.js
+++ React with Router/react-with-node/src/App.js
@@ -10,8 +10,35 @@ function App() {
   }, []);
   // console.log(users);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.name.value;
+    const user = { name, email };
+
+    // send data to server using POST
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+      });
+  };
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="name" />
+        <br />
+        <input type="email" name="email" id="email" />
+        <input type="submit" value="Submit" />
+      </form>
+
       {users?.map((user) => (
         <div key={user.id}>
           <h2>Id: {user.id}</h2>
